09240528bad65e405eb81fbc0b22922e3e29342f
Coolkishann <kishanv1464@gmail.com>
2026-04-02 22:30:44 +0530

    fix deploy issue-5

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/",
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://it-service-yzt6.onrender.com",
+    // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/",
 });
 
 // Attach JWT token to every request if present
